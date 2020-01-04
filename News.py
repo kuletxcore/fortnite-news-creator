281c648366cbdf05e203576b0edc8b454b924788
MyNameIsDark01 <48729254+MyNameIsDark01@users.noreply.github.com>
2020-01-04 22:59:57 +0100

    Add files via upload

---
+++ News.py
@@ -0,0 +1,349 @@
+import requests,json,textwrap
+import PIL
+from PIL import Image,ImageFont,ImageDraw
+from io import BytesIO
+import cv2
+import numpy as np
+import glob
+import os
+from util import ImageUtil, Utility
+from math import ceil
+
+TitleColor = (255,255,255)
+DescriptionColor = (51,236,254)
+
+def GetCreativeNews(Language = "en"):
+
+    FortniteGame = requests.get("https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game",headers={'Accept-Language' : Language.lower()}).json()["creativenews"]["news"]["motds"]
+
+    AvoidScam = "https://cdn2.unrealengine.com/Fortnite/fortnite-game/battleroyalenews/v42/BR04_MOTD_Shield-1024x512-75eacc957ecc88e76693143b6256ba06159efb76.jpg"
+
+    image_array = []
+
+    for filename in glob.glob('News/NewsCreative/*.png'):
+        os.remove(filename)
+
+    for index, Message in enumerate(FortniteGame):
+        news = News(Language, FortniteGame, Message)
+        news.save(f"News/NewsCreative/{index}.png")
+
+    for filename in glob.glob('News/NewsCreative/*.png'):
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width,height)
+        image_array.append(img)
+
+    out = cv2.VideoWriter('NewsCreative.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 0.3, size)
+
+    for i in range(len(image_array)):
+        out.write(image_array[i])
+    out.release
+
+def GetSTWNews(Language = "en"):
+
+    FortniteGame = requests.get("https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game",headers={'Accept-Language' : Language.lower()}).json()["savetheworldnews"]["news"]["messages"]
+
+    AvoidScam = "https://cdn2.unrealengine.com/Fortnite/fortnite-game/battleroyalenews/v42/BR04_MOTD_Shield-1024x512-75eacc957ecc88e76693143b6256ba06159efb76.jpg"
+
+    image_array = []
+
+    for filename in glob.glob('News/NewsSTW/*.png'):
+        os.remove(filename)
+
+    for index, Message in enumerate(FortniteGame):
+        news = News(Language, FortniteGame, Message)
+        news.save(f"News/NewsSTW/{index}.png")
+
+    for filename in glob.glob('News/NewsSTW/*.png'):
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width,height)
+        image_array.append(img)
+
+    out = cv2.VideoWriter('NewsSTW.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 0.3, size)
+
+    for i in range(len(image_array)):
+        out.write(image_array[i])
+    out.release
+
+def GetBRNews(Language = "it"):
+
+    FortniteGame = requests.get("https://fortnitecontent-website-prod07.ol.epicgames.com/content/api/pages/fortnite-game",headers={'Accept-Language' : Language.lower()}).json()["battleroyalenews"]["news"]["motds"]
+
+    AvoidScam = "https://cdn2.unrealengine.com/Fortnite/fortnite-game/battleroyalenews/v42/BR04_MOTD_Shield-1024x512-75eacc957ecc88e76693143b6256ba06159efb76.jpg"
+
+    image_array = []
+
+    for filename in glob.glob('News/NewsBR/*.png'):
+        os.remove(filename)
+
+    for index, Message in enumerate(FortniteGame):
+        news = News(Language, FortniteGame, Message)
+        news.save(f"News/NewsBR/{index}.png")
+
+    for filename in glob.glob('News/NewsBR/*.png'):
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width,height)
+        image_array.append(img)
+
+    out = cv2.VideoWriter('NewsBR.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 0.3, size)
+
+    for i in range(len(image_array)):
+        out.write(image_array[i])
+    out.release
+
+def News(Language, FortniteGame, Message):
+
+    if Language == "ja":
+        DescriptionFont = ImageFont.truetype('assets/Fonts/NotoSansJP-Bold.otf', 36)
+    elif Language == "ko":
+        DescriptionFont = ImageFont.truetype('assets/Fonts/NotoSansKR-Regular.otf', 36)
+    else:
+        DescriptionFont = ImageFont.truetype('assets/Fonts/burbanksmall-bold.otf', 36)
+
+    Background = Image.new("RGBA", (1920, 1080))
+    Draw = ImageDraw.Draw(Background)
+
+    msg = ""
+    TitleFontS = 100
+    Title = ""
+
+    if len(Message["body"]) > len(msg) and "body" in Message:
+        msg = Message["body"]
+
+    NewDesc = ""
+    for Desc in msg.split("\n"):
+        for Des in textwrap.wrap(Desc, width=39):
+            NewDesc += f'\n{Des}'
+
+    msg = NewDesc #Split the Description
+
+    Title = Message["title"].upper()
+    Description = Message["body"]
+
+    NewsImage = Image.open(BytesIO(requests.get(Message["image"]).content)) #Download the Imag
+    NewsImage = NewsImage.resize((1920, 1080), Image.ANTIALIAS) #Resize the downloaded Image
+
+    TitleFontSize = 60
+
+    if Language == "ja":
+        TitleFont = ImageFont.truetype('assets/Fonts/NIS_JYAU.otf', TitleFontSize)
+    elif Language == "ko":
+        TitleFont = ImageFont.truetype('assets/Fonts/AsiaERINM.otf', TitleFontSize)
+    else:
+        TitleFont = ImageFont.truetype('assets/Fonts/BurbankBigRegular-Black.otf', TitleFontSize)
+
+    NewDesc = ""
+    for Desc in Description.split("\n"):
+        for Des in textwrap.wrap(Desc, width=56):
+            NewDesc += f'\n{Des}'
+
+    Description = NewDesc #Split the Description
+
+    Background.paste(NewsImage,(0, 0)) #paste Background
+    Draw.text((50, 850),Title,TitleColor, font=TitleFont) #Draw Title
+
+    Draw.multiline_text((50, 870),Description,DescriptionColor,font=DescriptionFont,spacing=13) #Draw Description
+
+    TopTitle = TopUI(Language, FortniteGame)
+    if TopTitle is not None:
+        Background.paste(TopTitle, (0, 0))
+
+    return Background
+
+def TopUI(Language, FortniteGame):
+
+    TopUI = Image.new("RGB", (1920, 50))
+    canvas = ImageDraw.Draw(TopUI)
+
+    titles = []
+
+    for item in FortniteGame:
+        title = item["title"]
+        titles.append(title)
+
+    if len(titles) == 1:
+        rows = max(ceil(len(titles) / 3), ceil(len(titles) / 3))
+
+        i = 0
+        for item in FortniteGame:
+            card = GenerateCard1Titles(Language, item)
+            if card is not None:
+                TopUI.paste(
+                    card,
+                    ((0 + ((i % 3) * (card.width)), (0 + ((i // 3) * (card.height)))))
+                )
+
+                i = i + 1
+
+    elif len(titles) == 3:
+        rows = max(ceil(len(titles) / 3), ceil(len(titles) / 3))
+
+        i = 0
+        for item in FortniteGame:
+            card = GenerateCard3Titles(Language, item)
+            if card is not None:
+                TopUI.paste(
+                    card,
+                    ((0 + ((i % 3) * (card.width)), (0 + ((i // 3) * (card.height)))))
+                )
+
+                i = i + 1
+
+    elif len(titles) == 5:
+        rows = max(ceil(len(titles) / 5), ceil(len(titles) / 5))
+
+        i = 0
+        for item in FortniteGame:
+            card = GenerateCard5Titles(Language, item)
+            if card is not None:
+                TopUI.paste(
+                    card,
+                    ((0 + ((i % 5) * (card.width)), (0 + ((i // 5) * (card.height)))))
+                )
+
+                i = i + 1
+
+    return TopUI
+
+def GenerateCard1Titles(Language, item):
+
+    card = Image.new("RGBA", (1920, 50))
+
+    canvas = ImageDraw.Draw(card)
+
+    if Language == "ja":
+        font = ImageFont.truetype('assets/Fonts/NotoSansJP-Bold.otf', 15)
+    elif Language == "ko":
+        font = ImageFont.truetype('assets/Fonts/NotoSansKR-Regular.otf', 15)
+    else:
+        font = ImageFont.truetype('assets/Fonts/burbanksmall-black.otf', 15)
+
+    title = item["title"]
+    title = title.upper()
+
+    icon = item["image"]
+    icon = ImageUtil.Download(item, icon)
+    icon = ImageUtil.RatioResize(item, icon, 1920, 1080)
+    card.paste(icon, ImageUtil.CenterX(item, icon.width, card.width))
+
+    try:
+        TINT_COLOR = (0, 0, 205)  # Black
+        TRANSPARENCY = 0.7  # Degree of transparency, 0-100%
+        OPACITY = int(255 * TRANSPARENCY)
+
+        cardBottom = Image.new("RGBA", (1920,360), TINT_COLOR)
+        draw = ImageDraw.Draw(cardBottom)
+        draw.rectangle(((1920, 360), (0, 0)), fill=TINT_COLOR+(OPACITY,))
+    except Exception as e:
+        print(e)
+    card.paste(cardBottom, (0, 0), cardBottom)
+
+    textWidth, _ = font.getsize(title)
+    if textWidth >= 1920:
+        # Ensure that the item name does not overflow
+        font, textWidth = ImageUtil.FitTextX(item, title, 16, 1920)
+    canvas.text(
+        ImageUtil.CenterX(item, textWidth, card.width, 15),
+        title,
+        (255, 255, 255),
+        font=font,
+    )
+
+    return card
+
+def GenerateCard3Titles(Language, item):
+
+    card = Image.new("RGBA", (640, 50))
+
+    canvas = ImageDraw.Draw(card)
+
+    if Language == "ja":
+        font = ImageFont.truetype('assets/Fonts/NotoSansJP-Bold.otf', 15)
+    elif Language == "ko":
+        font = ImageFont.truetype('assets/Fonts/NotoSansKR-Regular.otf', 15)
+    else:
+        font = ImageFont.truetype('assets/Fonts/burbanksmall-black.otf', 15)
+
+    title = item["title"]
+    title = title.upper()
+
+    icon = item["image"]
+    icon = ImageUtil.Download(item, icon)
+    icon = ImageUtil.RatioResize(item, icon, 640, 360)
+    card.paste(icon, ImageUtil.CenterX(item, icon.width, card.width))
+
+    try:
+        TINT_COLOR = (0, 0, 205)  # Black
+        TRANSPARENCY = 0.7  # Degree of transparency, 0-100%
+        OPACITY = int(255 * TRANSPARENCY)
+
+        cardBottom = Image.new("RGBA", (640,360), TINT_COLOR)
+        draw = ImageDraw.Draw(cardBottom)
+        draw.rectangle(((640, 360), (0, 0)), fill=TINT_COLOR+(OPACITY,))
+    except Exception as e:
+        print(e)
+    card.paste(cardBottom, (0, 0), cardBottom)
+
+    textWidth, _ = font.getsize(title)
+    if textWidth >= 620:
+        # Ensure that the item name does not overflow
+        font, textWidth = ImageUtil.FitTextX(item, title, 16, 620)
+    canvas.text(
+        ImageUtil.CenterX(item, textWidth, card.width, 15),
+        title,
+        (255, 255, 255),
+        font=font,
+    )
+
+    return card
+
+def GenerateCard5Titles(Language, item):
+
+    card = Image.new("RGBA", (384, 50))
+
+    canvas = ImageDraw.Draw(card)
+
+    if Language == "ja":
+        font = ImageFont.truetype('assets/Fonts/NotoSansJP-Bold.otf', 15)
+    elif Language == "ko":
+        font = ImageFont.truetype('assets/Fonts/NotoSansKR-Regular.otf', 15)
+    else:
+        font = ImageFont.truetype('assets/Fonts/burbanksmall-black.otf', 15)
+
+    title = item["title"]
+    title = title.upper()
+
+    icon = item["image"]
+    icon = ImageUtil.Download(item, icon)
+    icon = ImageUtil.RatioResize(item, icon, 384, 216)
+    card.paste(icon, ImageUtil.CenterX(item, icon.width, card.width))
+
+    ### 1920 * 1080 = 384
+    try:
+        TINT_COLOR = (0, 0, 205)  # Black
+        TRANSPARENCY = 0.7  # Degree of transparency, 0-100%
+        OPACITY = int(255 * TRANSPARENCY)
+
+        cardBottom = Image.new("RGBA", (384,360), TINT_COLOR)
+        draw = ImageDraw.Draw(cardBottom)
+        draw.rectangle(((384, 360), (0, 0)), fill=TINT_COLOR+(OPACITY,))
+    except Exception as e:
+        print(e)
+    card.paste(cardBottom, (0, 0), cardBottom)
+
+    textWidth, _ = font.getsize(title)
+    if textWidth >= 364:
+        # Ensure that the item name does not overflow
+        font, textWidth = ImageUtil.FitTextX(item, title, 16, 364)
+    canvas.text(
+        ImageUtil.CenterX(item, textWidth, card.width, 15),
+        title,
+        (255, 255, 255),
+        font=font,
+    )
+
+    return card
+
+GetBRNews(Language = "it")
---
+++ util.py
@@ -0,0 +1,181 @@
+import json
+from datetime import datetime
+
+import requests
+from PIL import Image, ImageFont
+
+
+class Utility:
+    """Class containing utilitarian functions intended to reduce duplicate code."""
+
+    def GET(self, url: str, headers={}):
+        """
+        Return the response of a successful HTTP GET request to the specified
+        URL with the optionally provided header values.
+        """
+
+        res = requests.get(url, headers=headers)
+
+        # HTTP 200 (OK)
+        if res.status_code == 200:
+            return res.text
+        else:
+            print(f"Failed to GET {url} (HTTP {res.status_code})")
+
+    def Webhook(self, url: str, data: dict):
+        """POST the provided data to the specified Discord webhook url."""
+
+        headers = {"content-type": "application/json"}
+        data = json.dumps(data)
+
+        req = requests.post(url, headers=headers, data=data)
+
+        # HTTP 204 (No Content)
+        if req.status_code == 204:
+            return True
+        else:
+            return req.status_code
+
+    def nowISO(self):
+        """Return the current utc time in ISO8601 timestamp format."""
+
+        return datetime.utcnow().isoformat()
+
+    def ISOtoHuman(self, date: str):
+        """Return the provided ISO8601 timestamp in human-readable format."""
+
+        try:
+            # Unix-supported zero padding removal
+            return datetime.strptime(date, "%Y-%m-%d").strftime("%A, %B %-d, %Y")
+        except ValueError:
+            try:
+                # Windows-supported zero padding removal
+                return datetime.strptime(date, "%Y-%m-%d").strftime("%A, %B %#d, %Y")
+            except Exception as e:
+                print(f"Failed to convert to human-readable time, {e}")
+
+    def ReadFile(self, filename: str, extension: str, directory: str = "./FortniteZeroItalia/"):
+        """
+        Read and return the contents of the specified file.
+
+        Optionally specify a relative directory.
+        """
+
+        try:
+            with open(
+                f"{directory}{filename}.{extension}", "r", encoding="utf-8"
+            ) as file:
+                return file.read()
+        except Exception as e:
+            print(f"Failed to read {filename}.{extension}, {e}")
+
+
+class ImageUtil:
+    """Class containing utilitarian image-based functions intended to reduce duplicate code."""
+
+    def Open(self, filename: str, directory: str = "./FortniteZeroItalia/assets/images/"):
+        """Return the specified image file."""
+
+        return Image.open(f"{directory}{filename}")
+
+    def Download(self, url: str):
+        """Download and return the raw file from the specified url as an image object."""
+
+        res = requests.get(url, stream=True)
+
+        # HTTP 200 (OK)
+        if res.status_code == 200:
+            return Image.open(res.raw)
+        else:
+            print(f"Failed to GET {url} (HTTP {res.status_code})")
+
+    def RatioResize(self, image: Image.Image, maxWidth: int, maxHeight: int):
+        """Resize and return the provided image while maintaining aspect ratio."""
+
+        ratio = max(maxWidth / image.width, maxHeight / image.height)
+
+        return image.resize(
+            (int(image.width * ratio), int(image.height * ratio)), Image.ANTIALIAS
+        )
+
+    def CenterX(self, foregroundWidth: int, backgroundWidth: int, distanceTop: int = 0):
+        """Return the tuple necessary for horizontal centering and an optional vertical distance."""
+
+        return (int(backgroundWidth / 2) - int(foregroundWidth / 2), distanceTop)
+
+    def BottomX(self, foregroundWidth: int, backgroundWidth: int, distancetop: int = 0):
+        """Return the tuple necessary for horizontal centering and an optional vertical distance."""
+
+        return (int(backgroundWidth / 2) - int(foregroundWidth / 2), distancetop)
+    
+    def Font(
+        self,
+        size: int,
+        font: str = "BurbankBigCondensed-Black.otf",
+        directory: str = "./FortniteZeroItalia/assets/fonts/",
+    ):
+        """Return a font object with the specified font file and size."""
+
+        try:
+            return ImageFont.truetype(f"{directory}{font}", size)
+        except OSError:
+            print("BurbankBigCondensed-Black.otf not found, defaulted font to LuckiestGuy-Regular.ttf")
+
+            return ImageFont.truetype(f"{directory}LuckiestGuy-Regular.ttf", size)
+        except Exception as e:
+            print(f"Failed to load font, {e}")
+
+    def FitTextX(
+        self,
+        text: str,
+        size: int,
+        maxSize: int,
+        font: str = "BurbankBigCondensed-Black.otf",
+    ):
+        """Return the font and width which fits the provided text within the specified maxiumum width."""
+
+        font = ImageUtil.Font(self, size)
+        textWidth, _ = font.getsize(text)
+
+        while textWidth >= maxSize:
+            size = size - 1
+            font = ImageUtil.Font(self, size)
+            textWidth, _ = font.getsize(text)
+
+        return ImageUtil.Font(self, size), textWidth
+
+    def Font1(
+        self,
+        size: int,
+        font: str = "Burbank Big Condensed Bold.otf",
+        directory: str = "./FortniteZeroItalia/assets/fonts/",
+    ):
+        """Return a font object with the specified font file and size."""
+
+        try:
+            return ImageFont.truetype(f"{directory}{font}", size)
+        except OSError:
+            print("Burbank Big Condensed Bold.otf not found, defaulted font to LuckiestGuy-Regular.ttf")
+
+            return ImageFont.truetype(f"{directory}LuckiestGuy-Regular.ttf", size)
+        except Exception as e:
+            print(f"Failed to load font, {e}")
+
+    def FitTextX1(
+        self,
+        text: str,
+        size: int,
+        maxSize: int,
+        font: str = "Burbank Big Condensed Bold.otf",
+    ):
+        """Return the font and width which fits the provided text within the specified maxiumum width."""
+
+        font = ImageUtil.Font1(self, size)
+        textWidth, _ = font.getsize(text)
+
+        while textWidth >= maxSize:
+            size = size - 1
+            font = ImageUtil.Font1(self, size)
+            textWidth, _ = font.getsize(text)
+
+        return ImageUtil.Font1(self, size), textWidth
